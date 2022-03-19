cdb8e23a4374bd0f53b08512202822ef00856b9b
Shifanaaz02 <shifanaazsheikh02@gmail.com>
2022-03-19 23:43:32 +0530

    Basic js added

---
+++ game.js
@@ -0,0 +1,7 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[Math.floor(Math.random()*3)];
+var gamePattern = [];
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*3);
+
+}

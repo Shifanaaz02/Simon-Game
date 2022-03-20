a4b0dda34761b315026ff5a40b6e29234287da5d
Shifanaaz02 <shifanaazsheikh02@gmail.com>
2022-03-20 22:21:37 +0530

    Sounds added and animation

--- game.js
+++ game.js
@@ -1,7 +1,37 @@
+var userClickedPattern =[];
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour = buttonColours[Math.floor(Math.random()*3)];
 var gamePattern = [];
+$(".btn").click(function(event){
+    var userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    // var audio2 = new Audio('sounds/' + userChosenColour + '.mp3');
+    // audio2.play();
+});
 function nextSequence(){
-    var randomNumber = Math.floor(Math.random()*3);
-
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    // var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
+    // audio.play();
+    
+}
+function playSound(name){
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+}
+// function animatePress(currentColour){
+//     $("#"+currentColour).click(function(event){
+//         event.addClass("pressed");
+//     });
+// }
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#"+currentColour).removeClass("pressed");
+    }, 100);
 }
